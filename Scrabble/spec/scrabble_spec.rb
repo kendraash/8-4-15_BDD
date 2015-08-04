169b403c5bc61575c2bd6ccd737d5163a121b06f
Kyle Mellander & Kendra Ash <student@epicodus.com>
2015-08-04 11:52:27 -0700

    added error for special characters

--- Scrabble/spec/scrabble_spec.rb
+++ Scrabble/spec/scrabble_spec.rb
@@ -9,5 +9,16 @@ describe('String#scrabble') do
   it("returns a scrabble score for a word") do
     expect("kendra".scrabble()).to(eq(11))
   end
-  
+
+  it("returns value of a word with mixed case") do
+    expect("Kendra".scrabble()).to(eq(11))
+  end
+
+  it("returns an error message if there is spaces in between characters") do
+    expect("Ken dra".scrabble()).to(eq("Error: you can't do multiple words"))
+  end
+
+  it("returns an error message if there is a special character entered") do
+    expect("Kendra!".scrabble()).to(eq("Error: you can't do special characters"))
+  end
 end
