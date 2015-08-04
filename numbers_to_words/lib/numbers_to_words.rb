786983a1f7b3f6577af60bda15231db0c8badd34
Kyle Mellander & Kendra Ash <student@epicodus.com>
2015-08-04 14:33:33 -0700

    Added support for numbers up to 29

--- numbers_to_words/lib/numbers_to_words.rb
+++ numbers_to_words/lib/numbers_to_words.rb
@@ -1,10 +1,43 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    number_names = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19=>"nineteen"}
+    number_names = {
+      1 => "one",
+      2 => "two",
+      3 => "three",
+      4 => "four",
+      5 => "five",
+      6 => "six",
+      7 => "seven",
+      8 => "eight",
+      9 => "nine",
+      10 => "ten",
+      11 => "eleven",
+      12 => "twelve",
+      13 => "thirteen",
+      14 => "fourteen",
+      15 => "fifteen",
+      16 => "sixteen",
+      17 => "seventeen",
+      18 => "eighteen",
+      19 => "nineteen",
+      20 => "twenty"
+    }
+
+    number_array=self.to_s.split("")
     if self == 0
       "Zero"
     else
-      number_names[self].capitalize!()
+      if self <= 9
+        number_names[self].capitalize!()
+      elsif number_array.length()==2
+        if number_array[0]=="1"
+          number_names[self].capitalize!()
+        else
+          tens_place_value = number_names[number_array[0].to_i.*(10)]
+          ones_place_value = number_names[number_array[1].to_i]
+          tens_place_value.concat(" ").concat(ones_place_value).capitalize!()
+        end
+      end
     end
   end
 end
--- numbers_to_words/spec/numbers_to_words_spec.rb
+++ numbers_to_words/spec/numbers_to_words_spec.rb
@@ -5,7 +5,16 @@ describe("Fixnum#numbers_to_words") do
   it("returns a single digit number as a word description string") do
     expect(4.numbers_to_words).to(eq("Four"))
   end
-  it("returns a double digit number as a word description string")do
+
+  it("returns a double digit number in the teens as a word description string")do
     expect(15.numbers_to_words).to(eq("Fifteen"))
+  end
+
+  it("returns a double digit number that is in the twenties")do
+    expect(25.numbers_to_words).to(eq("Twenty five"))
+  end
+
+  it("returns a double digit number up to 99") do
+    expect(79.numbers_to_words).to(eq("Seventy nine"))
   end
 end
