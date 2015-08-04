b4266dbc1f0a58bce41ade6875ea266e7c025b07
Kyle Mellander & Kendra Ash <student@epicodus.com>
2015-08-04 14:34:05 -0700

    Added support for numbers up to 99

--- numbers_to_words/lib/numbers_to_words.rb
+++ numbers_to_words/lib/numbers_to_words.rb
@@ -20,7 +20,14 @@ class Fixnum
       17 => "seventeen",
       18 => "eighteen",
       19 => "nineteen",
-      20 => "twenty"
+      20 => "twenty",
+      30 => "thirty",
+      40 => "forty",
+      50 => "fifty",
+      60 => "sixty",
+      70 => "seventy",
+      80 => "eighty",
+      90 => "ninety"
     }
 
     number_array=self.to_s.split("")
